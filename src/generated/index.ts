b732724612d1a50d6a7ac3d55d2a2dcb489e12c7
Andre Kutianski <andre@nfe.io>
2026-01-19 18:47:05 -0300

    fix(generated): update last generated timestamps for multiple files

--- src/generated/index.ts
+++ src/generated/index.ts
@@ -5,7 +5,7 @@
  * Types are namespaced by spec to avoid conflicts.
  *
  * @generated
- * Last updated: 2026-01-19T01:51:48.790Z
+ * Last updated: 2026-01-19T21:43:26.518Z
  */
 
 // ============================================================================
